5cabd3a2d7c845381ffdb236c106360631f4eb3a
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-03-13 20:55:54 +0000

    Stub operator

--- order_frames.py
+++ order_frames.py
@@ -1,4 +1,27 @@
-print('ok')
+import bpy
+
+
+
+class SEQUENCE_OT_order_frames(bpy.types.Operator):
+    bl_idname = 'sequencer.order_frames'
+    bl_label = "Order text images"
+    bl_options = {'PRESET'}
+
+    target_name: bpy.props.StringProperty(name="Target", default='ordered')
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
+
+    def execute(self, context):
+        print('tons? target {}'.format(self.target_name))
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(SEQUENCE_OT_order_frames)
+
+def unregister():
+    bpy.utils.unregister_class(SEQUENCE_OT_order_frames)
 
 if __name__ == '__main__':
-    print('main!')
+    register()
