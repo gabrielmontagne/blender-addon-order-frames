a9bf94245b304c2d6283848d5849dd448c5e7aa5
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-03-14 11:06:28 +0000

    copy frames, setup strip; min cleanup

--- order_frames.py
+++ order_frames.py
@@ -4,9 +4,16 @@ import bpy
 import os
 from bpy.path import relpath, clean_name, abspath
 
+bl_info = {
+    'name': "VSE: Order image sequences",
+    'category': 'Development',
+    'author': "Gabriel Montagné Láscaris-Comneno",
+    'blender': (2, 80, 0)
+}
+
 class SEQUENCE_OT_order_frames(bpy.types.Operator):
     bl_idname = 'sequencer.order_frames'
-    bl_label = "Order text images"
+    bl_label = "Order image sequences"
     bl_options = {'PRESET'}
 
     target_name: bpy.props.StringProperty(name="Target", default='ordered')
@@ -47,7 +54,9 @@ class SEQUENCE_OT_order_frames(bpy.types.Operator):
         for i, element in enumerate(all_elements):
 
             extension = splitext(element)[1]
-            destination_path = join(target_path, 'frame-{:08d}.{}'.format(i, extension))
+            file_name = 'frame-{:08d}{}'.format(i, extension)
+            destination_path = join(target_path, file_name)
+            copy(element, destination_path)
 
             if i == 0:
                 first_strip = ordered_sequences[0]
@@ -55,14 +64,11 @@ class SEQUENCE_OT_order_frames(bpy.types.Operator):
                 print('Creating new image sequence "{}"'.format(new_sequence_name))
                 new_sequence = context.scene.sequence_editor.sequences.new_image(
                     name=new_sequence_name,
-                    filepath=relpath(element),
+                    filepath=relpath(destination_path),
                     frame_start=first_strip.frame_final_start,
-                    channel=first_strip.channel + 1)
+                    channel=active_strip.channel + 1)
             else:
-                new_sequence.elements.append(split(element)[1])
-
-        # window_manager.progress_begin(0, len(elements_to_process))
-        # print('elements_to_process', elements_to_process)
+                new_sequence.elements.append(file_name)
 
         return {'FINISHED'}
 
